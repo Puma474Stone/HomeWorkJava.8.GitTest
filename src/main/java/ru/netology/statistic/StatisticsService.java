39b644eae42f242404069fcd85b67dad450a4280
Puma474Stone <shustov.arcadi@yandex.ru>
2024-06-12 15:52:58 +0700

    Full testing

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -10,13 +10,6 @@ public class StatisticsService {
         }
         return currentMax;
     }
-    public long findMin(long[] incomes) {
-        long currentMin = incomes[0];
-        for (long income : incomes) {
-            if (currentMin > income) {
-                currentMin = income;
-            }
-        }
-        return currentMin;
-    }
 }
+
+
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
 
@@ -17,15 +16,68 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    void findMin() {
+    void findMaxWithAllPositiveNumbers() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
+        long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        long expected = 10;
 
-        long actual = service.findMin(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
     }
-}
+
+    @Test
+    void findMaxWithNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10};
+        long expected = -1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithMixedNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-10, 15, -5, 20, 0};
+        long expected = 20;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithAllSameNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+        long expected = 5;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithSingleElement() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {42};
+        long expected = 42;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    }
+
+
